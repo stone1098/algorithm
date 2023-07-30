1a803e29d9958ee1392fd8f71765d2b7bf3ec0d3
Shin <stone1098@naver.com>
2023-07-30 15:00:58 +0900

    boj_12865

--- Dynamic Programming/boj_12865.py
+++ Dynamic Programming/boj_12865.py
@@ -1,22 +1,13 @@
 # https://www.acmicpc.net/problem/12865
 
-N, K = 4, 7
-wv = [
-    [0, 0],
-    [6, 13],
-    [4, 8],
-    [3, 6],
-    [5, 12],
-]
+import sys
 
-# import sys
+N, K = map(int, sys.stdin.readline().split())
 
-# N, K = map(int, sys.stdin.readline().split())
-
-# wv = [[0, 0]]
-# for _ in range(N):
-#     w, v = map(int, sys.stdin.readline().split())
-#     wv.append([w, v])
+wv = [[0, 0]]
+for _ in range(N):
+    w, v = map(int, sys.stdin.readline().split())
+    wv.append([w, v])
 
 dp = [[0] * (K+1) for _ in range(N+1)] # N * K 배열 구성
 
