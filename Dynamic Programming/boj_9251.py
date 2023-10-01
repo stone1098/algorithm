57f24a64875b8ca9d8280de86b02e80527f0c207
Shin <stone1098@naver.com>
2023-10-01 17:29:46 +0900

    boj_9251.py

--- Dynamic Programming/boj_9251.py
+++ Dynamic Programming/boj_9251.py
@@ -37,3 +37,22 @@ for i in range(1, row+1): # 행
             dp[i][j] = max(dp[i][j-1], dp[i-1][j])
 
 print(dp[row][column])
+
+######장수정 답####
+import sys
+input = sys.stdin.readline
+
+s1 = input().strip()
+s2 = input().strip()
+dp = [0] * 1000
+# dp 각 문자별로 다른 문자열을 순회하면서
+# 같은 문자면 + 1, 다른 경우 누적값이 해당 위치 dp보다 작으면 갱신
+for i in range(len(s1)):
+    mx = 0
+    for j in range(len(s2)):
+        if mx < dp[j]:
+            mx = dp[j]
+        elif s1[i] == s2[j]:
+            dp[j] = mx + 1
+
+print(max(dp))
