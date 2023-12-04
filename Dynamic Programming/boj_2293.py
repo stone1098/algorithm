3b1d7e51a608c3e1eeeb22782d7270fe35792873
Shin <stone1098@naver.com>
2023-12-04 19:38:03 +0900

    boj_2293.py

---
+++ Dynamic Programming/boj_2293.py
@@ -0,0 +1,33 @@
+# https://www.acmicpc.net/problem/2293
+
+'''
+n가지 종류의 동전이 있다. 각각의 동전이 나타내는 가치는 다르다. 
+이 동전을 적당히 사용해서, 그 가치의 합이 k원이 되도록 하고 싶다. 
+그 경우의 수를 구하시오. 각각의 동전은 몇 개라도 사용할 수 있다.
+
+사용한 동전의 구성이 같은데, 순서만 다른 것은 같은 경우이다.
+
+첫째 줄에 n, k가 주어진다. (1 ≤ n ≤ 100, 1 ≤ k ≤ 10,000)
+다음 n개의 줄에는 각각의 동전의 가치가 주어진다. 동전의 가치는 100,000보다 작거나 같은 자연수이다.
+
+첫째 줄에 경우의 수를 출력한다. 경우의 수는 2^31보다 작다.
+'''
+
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+coins = []
+for _ in range(n):
+    coins.append(int(input()))
+
+dp = [0] * 20000
+
+for coin in coins:
+    if coin <= k:
+        dp[coin] += 1
+
+        for i in range(coin+1, k+1):
+            dp[i] += dp[i-coin]
+
+print(dp[k])
